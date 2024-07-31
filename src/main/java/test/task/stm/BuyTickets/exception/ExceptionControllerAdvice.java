899a1a6db48a296dd45f737d3556bf6651ec09c2
Juliia228 <miss-komarova2017@yandex.ru>
2024-07-31 19:08:55 +0300

    Add exception handling

---
+++ src/main/java/test/task/stm/BuyTickets/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,67 @@
+package test.task.stm.BuyTickets.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKeyException(DuplicateKeyException duplicateKeyException, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + duplicateKeyException);
+        ErrorResponse errorResponse = new ErrorResponse("Не удалось добавить объект", duplicateKeyException.getCause().getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException dataAccessException, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + dataAccessException);
+        ErrorResponse errorResponse = new ErrorResponse("Не удалось найти объект", dataAccessException.getMessage());
+        return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + exception);
+        ErrorResponse errorResponse = new ErrorResponse("Ошибка в параметрах запроса", exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDataNotFoundException(DataNotFoundException dataNotFoundException, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + dataNotFoundException);
+        ErrorResponse errorResponse = new ErrorResponse("Данные не найдены", dataNotFoundException.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(RegistrationException.class)
+    public ResponseEntity<ErrorResponse> handleRegistrationException(RegistrationException registrationException, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + registrationException);
+        ErrorResponse errorResponse = new ErrorResponse("Регистрация этого пользователя невозможна", registrationException.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidDataAccessApiUsageException(InvalidDataAccessApiUsageException exception, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + exception);
+        ErrorResponse errorResponse = new ErrorResponse("Невозможно сохранить объект", exception.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException badRequestException, HttpServletRequest request) {
+        log.error(request.getRequestURL() + " raised " + badRequestException);
+        ErrorResponse errorResponse = new ErrorResponse("Невозможно выполнить запрос", badRequestException.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
---
+++ src/main/java/test/task/stm/BuyTickets/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package test.task.stm.BuyTickets.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
