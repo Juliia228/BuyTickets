e995af3d6a57c789654df158161cef98b9348190
Juliia228 <miss-komarova2017@yandex.ru>
2024-08-01 15:50:28 +0300

    Added validation

--- src/main/java/test/task/stm/BuyTickets/models/BuyTicketRequest.java
+++ src/main/java/test/task/stm/BuyTickets/models/BuyTicketRequest.java
@@ -1,9 +1,12 @@
 package test.task.stm.BuyTickets.models;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class BuyTicketRequest {
+    @Min(value = 1)
     private int user_id;
+    @Min(value = 1)
     private int ticket_id;
 }
