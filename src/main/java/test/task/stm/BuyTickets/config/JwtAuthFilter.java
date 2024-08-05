21418289fdab3c84f8c3fabc376060523e67e788
Juliia228 <miss-komarova2017@yandex.ru>
2024-08-05 21:22:25 +0300

    Invalid token returns 401 status

--- src/main/java/test/task/stm/BuyTickets/config/JwtAuthFilter.java
+++ src/main/java/test/task/stm/BuyTickets/config/JwtAuthFilter.java
@@ -1,9 +1,14 @@
 package test.task.stm.BuyTickets.config;
 
+import io.jsonwebtoken.ClaimJwtException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +22,7 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     public static final String BEARER_PREFIX = "Bearer ";
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
@@ -31,21 +37,27 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
-        // получаем токен из заголовка и имя пользователя из токена
-        if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
-            token = authHeader.substring(BEARER_PREFIX.length());
-            username = jwtService.extractUsername(token);
-        }
-
-        // аутентифицируем пользователя, если токен валидный
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            if (jwtService.isTokenValid(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        try {
+            // получаем токен из заголовка и имя пользователя из токена
+            if (authHeader != null && authHeader.startsWith(BEARER_PREFIX)) {
+                token = authHeader.substring(BEARER_PREFIX.length());
+                username = jwtService.extractUsername(token);
             }
+
+            // аутентифицируем пользователя, если токен валидный
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (JwtException exception) {
+            log.error(request.getRequestURL() + " raised " + exception);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
-        filterChain.doFilter(request, response);
     }
 }
