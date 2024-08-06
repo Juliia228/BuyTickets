d1d43d3ad66a5fa13b727b1e62f3639650f4afc0
Juliia228 <miss-komarova2017@yandex.ru>
2024-08-06 18:09:53 +0300

    Added authentication to Swagger

--- src/main/java/test/task/stm/BuyTickets/config/SwaggerConfig.java
+++ src/main/java/test/task/stm/BuyTickets/config/SwaggerConfig.java
@@ -1,18 +1,30 @@
 package test.task.stm.BuyTickets.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Buy Tickets Api",
-                description = "API для покупки транспортных билетов", version = "1.0.0",
-                contact = @Contact(
-                        name = "Комарова Юлия",
-                        url = "https://web.telegram.org/k/#@iuliia_kom"
-                )
-        )
-)
+@Configuration
 public class SwaggerConfig {
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Buy Tickets Api")
+                        .description("API для покупки транспортных билетов")
+                        .version("1.0.0")
+                        .contact(new Contact().name("Комарова Юлия").url("https://web.telegram.org/k/#@iuliia_kom")));
+    }
 }
